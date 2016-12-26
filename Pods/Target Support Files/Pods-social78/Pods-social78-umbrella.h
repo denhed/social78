039b5628f47ab1e104f396c5f58ea967a809c1ab
Dennis Hedlund <denhed@Denniss-iMac.local>
2016-12-26 20:17:00 +0100

    Completed setting up Firebase integration

---
+++ Pods/Target Support Files/Pods-social78/Pods-social78-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_social78VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_social78VersionString[];
+
---
+++ social78/SignInVC.swift
@@ -0,0 +1,24 @@
+//
+//  SignInVC.swift
+//  social78
+//
+//  Created by Dennis Hedlund on 2016-12-26.
+//  Copyright © 2016 Dennis Hedlund. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
