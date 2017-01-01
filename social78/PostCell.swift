b2b6df5e9deea0e0f92736ba20cf3bf34e8c079a
Dennis Hedlund <denhed@Denniss-iMac.local>
2017-01-01 12:47:25 +0100

    Completed Firebase Display

--- social78/PostCell.swift
+++ social78/PostCell.swift
@@ -13,13 +13,20 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var profileImg: UIImageView!
     @IBOutlet weak var usernameLbl: UILabel!
     @IBOutlet weak var postImg: UIImageView!
-    @IBOutlet weak var caption: UILabel!
+    @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
+    
+    var post: Post!
     
     override func awakeFromNib() {
         super.awakeFromNib()
     
     }
     
+    func configureCell(post: Post) {
+        self.post = post
+        self.caption.text = post.caption
+        self.likesLbl.text = "\(post.likes)"
+    }
     
 }
